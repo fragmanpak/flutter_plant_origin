cdd5f8934cbdfd30f4740acddc1c24d57ba72ecb
waqar-zam <waqarzemstudio@gmail.com>
2023-02-28 07:57:36 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/blindbugs/flutterplanetstutorial/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.blindbugs.flutterplanetstutorial
+
+import android.os.Bundle
+
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
